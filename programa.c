d9bd015baf882c51fac3580759ff360e39fb870c
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-09-24 20:09:16 +0000

    Criada as funcoes MenuZero e EscolhaOpcao

--- programa.c
+++ programa.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    
+void MenuZero() {
+  printf("--------------------------------------------------------\n");
+  printf("Menu 0\n");
+  printf("--------------------------------------------------------\n");
+  printf("Deseja sair do jogo S Sim / N Nao?\n");
+  char opcao;
+  scanf("%c", &opcao);
+  fflush(stdin);
+  if (opcao == 'S') {
+    //system("0");
+  }else {
+      
+  }
+  
+}
+
+int EscolhaOpcao() {
   printf("--------------------------------------------------------\n");
   printf("|                    MENU DE OPCOES                    |\n");
   printf("|  0 - SAIR DO GAME                                    |\n");
@@ -13,6 +29,21 @@ int main() {
   printf("|  6 - VER OS DESENVOLVEDORES                          |\n");
   printf("|  7 - VER PONTUACAO                                   |\n");
   printf("--------------------------------------------------------\n");
-  printf("O QUE DESEJA FAZER ?         \n");
+  printf("O QUE DESEJA FAZER ?                                    \n");
+  int nivel;
+  scanf("%d", &nivel);
+  system("clear");
+  return nivel;
+}
 
+int main() {
+  int opcao = EscolhaOpcao();
+  switch (opcao) {
+  case 0:
+    MenuZero();
+    break;
+
+  default:
+    printf("VOCE DIGITOU ALGO ERRADO!\n");
+  }
 }
