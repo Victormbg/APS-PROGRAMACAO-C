83b839630425517ce394b7da3faf532c63c2da3a
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-09-24 14:39:58 +0000

    adicionado o menu

--- programa.c
+++ programa.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World\n");
+  printf("--------------------------------------------------------\n");
+  printf("|                    MENU DE OPCOES                    |\n");
+  printf("|  0 - SAIR DO GAME                                    |\n");
+  printf("|  1 - JOGAR TODOS OS NIVEIS - GAME COMPLETO COM MEMES |\n");
+  printf("|  2 - JOGAR NIVEL FACIL - SOMENTE O NIVEL FACIL       |\n");
+  printf("|  3 - JOGAR NIVEL MEDIO - SOMENTE O NIVEL MEDIO       |\n");
+  printf("|  4 - JOGAR NIVEL DIFICIL - SOMENTE O NIVEL DIFICIL   |\n");
+  printf("|  5 - VER INSTRUCOES DO JOGO E INFORMACOES            |\n");
+  printf("|  6 - VER OS DESENVOLVEDORES                          |\n");
+  printf("|  7 - VER PONTUACAO                                   |\n");
+  printf("--------------------------------------------------------\n");
+  printf("O QUE DESEJA FAZER ?         \n");
+
 }
