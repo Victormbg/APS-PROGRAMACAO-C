38e49d6fa4e14c0729bf5ac024139995c83d78c4
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-10-03 12:50:40 +0000

    feito sistema de pontuacao para menuDois

--- programa.c
+++ programa.c
@@ -2,6 +2,9 @@
 #include <stdlib.h> // Fazer o system("clear || cls") funciona
 #include <unistd.h> // Fazer o sleep(3) funcionar
 
+int certo_1 = 0, errado_1 = 0;
+char nome[100];
+
 void EscolhaOpcao() {
   printf("--------------------------------------------------------\n");
   printf("|                    MENU DE OPCOES                    |\n");
@@ -37,7 +40,7 @@ void MenuZero() {
 }
 
 void MenuCinco() {
-  system ("clear || cls");
+  system("clear || cls");
   printf("----------------------------------\n");
   printf("Menu 5 \n");
   printf("----------------------------------\n\n");
@@ -60,8 +63,10 @@ void MenuCinco() {
   printf("Nivel dificil - Cada resposta certa valera 250 pontos\n");
   printf("Basicamente eh isso, divirta-se.           \n");
   printf("----------------------------------\n");
-  getchar();printf("PRESSIONE ENTER PARA VOLTAR...");
-  getchar();system ("clear || cls");
+  getchar();
+  printf("PRESSIONE ENTER PARA VOLTAR...");
+  getchar();
+  system("clear || cls");
 }
 
 void MenuSeis() {
@@ -73,129 +78,161 @@ void MenuSeis() {
   printf("Victor Manuel de Barros Garcia\n");
   printf("Marcos Paulo Carneiro Tavares\n");
   printf("Cainã James Moreira\n\n");
-  getchar();printf("PRESSIONE ENTER PARA VOLTAR...");
-  getchar();system ("clear || cls");
+  getchar();
+  printf("PRESSIONE ENTER PARA VOLTAR...");
+  getchar();
+  system("clear || cls");
 }
 
-int MenuDois(){
-    char nome[100];
-    char resp;
-    int certo_1 = 0,errado_1 = 0;
-    // Limpar tela no terminal 
-    system ("clear || cls");
-    printf("DIGITE O NOME DO PARTICIPANTE: ");
-    scanf("%s", nome);
-    // Limpar tela no terminal
-    system ("clear || cls");
-    printf("SEJA BEM VINDO %s\n\n",nome);    
-    printf("Este e um jogo de perguntas e respostas. O jogador devera escolher,\n");
-    printf("dentre as 4 alternativas apenas uma e em seguida pressionar enter\n");
-    printf("Para mais informacoes, acesse a opcao intrucoes do menu do jogo.\n\n");
-    getchar();printf("PRESSIONE ENTER PARA COMECAR...");
-    getchar();system ("clear || cls");
-    system("clear || cls");
-    //Troca a cor do console "2 verde 7 branco"
-    system("color 27");
-    printf("*===============================================================================*\n");
-    printf("|  ___   ___ ___ ___	___ _________ ___									    |\n");
-    printf("|  XXX_  XXX XXX XXX	XXX XXXXXXXXX XXX									    |\n");
-    printf("|  XXXX_ XXX XXX XXX	XXX XXXXXXXXX XXX								    	|\n");
-    printf("|  XXXXX_XXX XXX XXX_  _XXX XXX____   XXX								     	|\n");
-    printf("|  XXXXXXXXX XXX  XXX__XXX  XXXXXXX   XXX									    |\n");
-    printf("|  XXX XXXXX XXX   XXXXXX   XXX______ XXX______						     	    |\n");
-    printf("|  XXX  XXXX XXX	XXXX	XXXXXXXXX XXXXXXXXX							        |\n");
-    printf("|  XXX   XXX XXX	 XX	    XXXXXXXXX XXXXXXXXX							        |\n");
-    printf("|								_________ _________ _________ ___ ___	     	|\n");
-    printf("|								XXXXXXXXX XXXXXXXXX XXXXXXXXX XXX XXX		    |\n");
-    printf("|								XXXXXXXXX XXXXXXXXX XXXXXXXXX XXX XXX		    |\n");
-    printf("|								XXX___	  XXX___XXX XXX	      XXX XXX           |\n");
-    printf("|								XXXXXX	  XXXXXXXXX XXX	      XXX XXX		    |\n");
-    printf("|								XXXXXX	  XXXXXXXXX XXX______ XXX XXX______     |\n");
-    printf("|								XXX	      XXX   XXX XXXXXXXXX XXX XXXXXXXXX     |\n");
-    printf("|								XXX	      XXX   XXX XXXXXXXXX XXX XXXXXXXXX     |\n");
-    printf("|																			    |\n");
-    printf("*=============================================================================*\n\n");
-    printf("			  AS PERGUNSTAS DE NIVEL \"FACIL\" VALEM 2 PONTOS CADA             \n\n");
-    printf ("Pressione \"enter\" para continuar...");
-    // Limpar tela no terminal
-    getchar();system ("clear || cls");
-    //Troca a cor do console "0 preto 7 branco"
-    system("color 07");
-    printf ("*--------------------------------*\n");
-    printf ("|  Pergunta 1 - Nivel Facil  |\n");
-    printf ("*--------------------------------*\n\n");
-    printf ("O que significa CPU?\n");
-    printf ("-------------------------------------------------------------------------------\n");
-    printf ("A) Control Panel Unit - Unidade de Painel de Controle \n");
-    printf ("B) Central Power Unit - Unidade de Força Central\n");
-    printf ("C) Control Power Unit - Unidade de Controle de Energia\n");
-    printf ("D) Central Processing Unit - Unidade Central de Processamento\n");
-    printf ("-------------------------------------------------------------------------------\n");
-    printf ("Qual a resposta certa?\n");
-    scanf ("%s",&resp);
-    if ((resp=='d')||(resp='D'))
-    {
-        printf ("Resposta correta\n");
-        certo_1++;
-    }
-    else
-    {
-        printf ("Resposta errada\n");
-        printf ("Resposta correta: Letra \"D\"\n");
-        errado_1++;
-    }
-    printf ("-------------------------------------------------------------------------------\n");
-    getchar();printf ("Pressione \"enter\" para continuar...");
-    getchar();system("clear || cls");
-    printf ("*--------------------------------*\n");
-    printf ("|  Pergunta 2 - Nivel Facil   |\n");
-    printf ("*--------------------------------*\n\n");
-    printf ("Quais são as principais fabricantes de processadores para Desktops e notebooks ?\n");
-    printf ("-------------------------------------------------------------------------------\n");
-    printf ("A) Intel e Dell \n");
-    printf ("B) Dell e AMD \n");
-    printf ("C) Intel e AMD \n");
-    printf ("D) AMD e IBM \n");
-    printf ("-------------------------------------------------------------------------------\n");
-    printf ("Qual a resposta certa?\n");
-    scanf ("%s",&resp);
-    if ((resp=='c')||(resp=='C'))
-    {
-        printf ("Resposta correta\n");
-        certo_1++;
-    }
-    else
-    {
-        printf ("Resposta errada\n");
-        printf ("Resposta correta: Letra \"C\"\n");
-        errado_1++;
-    }
-    printf ("Pressione \"enter\" para continuar...");
-    getchar();system ("clear || cls");
-    return certo_1;
+void MenuDois() {
+  char resp;
+  // Limpar tela no terminal
+  system("clear || cls");
+  printf("DIGITE O NOME DO PARTICIPANTE: ");
+  scanf("%s", nome);
+  // Limpar tela no terminal
+  system("clear || cls");
+  printf("SEJA BEM VINDO %s\n\n", nome);
+  printf(
+      "Este e um jogo de perguntas e respostas. O jogador devera escolher,\n");
+  printf("dentre as 4 alternativas apenas uma e em seguida pressionar enter\n");
+  printf(
+      "Para mais informacoes, acesse a opcao intrucoes do menu do jogo.\n\n");
+  getchar();
+  printf("PRESSIONE ENTER PARA COMECAR...");
+  getchar();
+  system("clear || cls");
+  system("clear || cls");
+  // Troca a cor do console "2 verde 7 branco"
+  system("color 27");
+  printf("*===================================================================="
+         "===========*\n");
+  printf("|  ___   ___ ___ ___	___ _________ ___			"
+         "						    |\n");
+  printf("|  XXX_  XXX XXX XXX	XXX XXXXXXXXX XXX			"
+         "						    |\n");
+  printf("|  XXXX_ XXX XXX XXX	XXX XXXXXXXXX XXX			"
+         "					    	|\n");
+  printf("|  XXXXX_XXX XXX XXX_  _XXX XXX____   XXX			"
+         "					     	|\n");
+  printf("|  XXXXXXXXX XXX  XXX__XXX  XXXXXXX   XXX			"
+         "						    |\n");
+  printf("|  XXX XXXXX XXX   XXXXXX   XXX______ XXX______		"
+         "				     	    |\n");
+  printf("|  XXX  XXXX XXX	XXXX	XXXXXXXXX XXXXXXXXX		"
+         "					        |\n");
+  printf("|  XXX   XXX XXX	 XX	    XXXXXXXXX XXXXXXXXX		"
+         "					        |\n");
+  printf("|								"
+         "_________ _________ _________ ___ ___	     	|\n");
+  printf("|								"
+         "XXXXXXXXX XXXXXXXXX XXXXXXXXX XXX XXX		    |\n");
+  printf("|								"
+         "XXXXXXXXX XXXXXXXXX XXXXXXXXX XXX XXX		    |\n");
+  printf("|								"
+         "XXX___	  XXX___XXX XXX	      XXX XXX           |\n");
+  printf("|								"
+         "XXXXXX	  XXXXXXXXX XXX	      XXX XXX		    |\n");
+  printf("|								"
+         "XXXXXX	  XXXXXXXXX XXX______ XXX XXX______     |\n");
+  printf("|								"
+         "XXX	      XXX   XXX XXXXXXXXX XXX XXXXXXXXX     |\n");
+  printf("|								"
+         "XXX	      XXX   XXX XXXXXXXXX XXX XXXXXXXXX     |\n");
+  printf("|								"
+         "								"
+         "			    |\n");
+  printf("*===================================================================="
+         "=========*\n\n");
+  printf("			  AS PERGUNSTAS DE NIVEL \"FACIL\" VALEM 2 "
+         "PONTOS CADA             \n\n");
+  printf("Pressione \"enter\" para continuar...");
+  // Limpar tela no terminal
+  getchar();
+  system("clear || cls");
+  // Troca a cor do console "0 preto 7 branco"
+  system("color 07");
+  printf("*--------------------------------*\n");
+  printf("|  Pergunta 1 - Nivel Facil  |\n");
+  printf("*--------------------------------*\n\n");
+  printf("O que significa CPU?\n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("A) Control Panel Unit - Unidade de Painel de Controle \n");
+  printf("B) Central Power Unit - Unidade de Força Central\n");
+  printf("C) Control Power Unit - Unidade de Controle de Energia\n");
+  printf("D) Central Processing Unit - Unidade Central de Processamento\n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("Qual a resposta certa?\n");
+  scanf("%s", &resp);
+  if ((resp == 'd') || (resp = 'D')) {
+    printf("Resposta correta\n");
+    certo_1++;
+  } else {
+    printf("Resposta errada\n");
+    printf("Resposta correta: Letra \"D\"\n");
+    errado_1++;
+  }
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  getchar();
+  printf("Pressione \"enter\" para continuar...");
+  getchar();
+  system("clear || cls");
+  printf("*--------------------------------*\n");
+  printf("|  Pergunta 2 - Nivel Facil   |\n");
+  printf("*--------------------------------*\n\n");
+  printf("Quais são as principais fabricantes de processadores para Desktops e "
+         "notebooks ?\n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("A) Intel e Dell \n");
+  printf("B) Dell e AMD \n");
+  printf("C) Intel e AMD \n");
+  printf("D) AMD e IBM \n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("Qual a resposta certa?\n");
+  scanf("%s", &resp);
+  if ((resp == 'c') || (resp == 'C')) {
+    printf("Resposta correta\n");
+    certo_1++;
+  } else {
+    printf("Resposta errada\n");
+    printf("Resposta correta: Letra \"C\"\n");
+    errado_1++;
+  }
+  getchar();
+  printf("\n\nPRESSIONE ENTER PARA VOLTAR...");
+  getchar();
+  system("clear || cls");
 }
 
-void MenuSete(){
-    //int res = MenuDois(certo_1);
-    //int ponto = res * 150;
-    system("clear || cls");
-    printf("----------------------------------\n");
-    printf("Menu 7 \n");
-    printf("----------------------------------\n\n");  
-    printf("RESULTADOS DO JOGO \n\n");
-    printf("PONTUACAO DO NIVEL  XXXX \n");
-    printf("Participante:  nome \n\n");
-    //printf("Total de acertos  : %d\n",res);
-    printf("Total de erros    : \n");
-    //printf("Pontuacao total   : %d\n\n",ponto);
-    getchar();printf("PRESSIONE ENTER PARA VOLTAR...");
-    getchar();system ("clear || cls");
+void MenuSete() {
+  int resAcerto = certo_1;
+  int ponto = certo_1 * 150;
+  system("clear || cls");
+  printf("----------------------------------\n");
+  printf("Menu 7 \n");
+  printf("----------------------------------\n\n");
+  printf("RESULTADOS DO JOGO \n\n");
+  printf("PONTUACAO DO NIVEL  XXXX \n");
+  printf("Participante: %s\n\n", nome);
+  printf("Total de acertos  : %d\n", resAcerto);
+  printf("Total de erros    : \n");
+  printf("Pontuacao total   : %d\n\n", ponto);
+  getchar();
+  printf("PRESSIONE ENTER PARA VOLTAR...");
+  getchar();
+  system("clear || cls");
 }
 
 int main() {
+  system("clear || cls");
   int nivel = 1;
-  do{
-    EscolhaOpcao();  
+  do {
+    EscolhaOpcao();
     scanf("%d", &nivel);
     switch (nivel) {
     case 0:
@@ -216,5 +253,5 @@ int main() {
     default:
       printf("VOCE DIGITOU ALGO ERRADO!\n");
     }
-    }while (nivel);  
+  } while (nivel);
 }
