3b8ce7f28481fddfa7f22db9c18532336970d88d
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-10-05 12:17:44 +0000

    terminado as perguntas faceis

--- programa.c
+++ programa.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h> // Fazer o system("clear || cls") funciona
-#include <unistd.h> // Fazer o sleep(3) funcionar
 #include <string.h>
+#include <unistd.h> // Fazer o sleep(3) funcionar
 
 int certo_1 = 0, errado_1 = 0;
 char nome[100];
@@ -208,7 +208,220 @@ void MenuDois() {
     errado_1++;
   }
   getchar();
-  printf("\n\nPRESSIONE ENTER PARA VOLTAR...");
+  printf("Pressione \"enter\" para continuar...");
+  getchar();
+  system("clear || cls");
+
+  printf("--------------------------------\n");
+  printf("|  Pergunta 3 - Nivel Facil   |\n");
+  printf("--------------------------------\n\n");
+  printf("Quais são os sintomas mais comuns de superaquecimento do "
+         "processador?\n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("A) Apitos agudos da placa-mãe\n");
+  printf("B) Reinicios e cogelamentos inesperados da maquina \n");
+  printf("C) Cheiro de queimado que exala de dentro do gabinete \n");
+  printf("D) Imagem corrompida, com cores trocadas e manchas \n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("Qual a resposta certa?\n");
+  scanf("%s", &resp);
+  if ((resp == 'b') || (resp == 'B')) {
+    printf("Resposta correta\n");
+    certo_1++;
+  } else {
+    printf("Resposta errada\n");
+    printf("Resposta correta: Letra \"C\"\n");
+    errado_1++;
+  }
+  getchar();
+  printf("Pressione \"enter\" para continuar...");
+  getchar();
+  system("clear || cls");
+  printf("--------------------------------\n");
+  printf("|  Pergunta 4 - Nivel Facil   |\n");
+  printf("--------------------------------\n\n");
+  printf("O que quer dizer bug ?\n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("A) E a memoria que facilita as tarefas e liberta o processador. \n");
+  printf("B) E um tipo de periferico \n");
+  printf("C) E um erro de programacao \n");
+  printf("D) E uma peca de hardware  \n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("Qual a resposta certa?\n");
+  scanf("%s", &resp);
+  if ((resp == 'c') || (resp == 'C')) {
+    printf("Resposta correta\n");
+    certo_1++;
+  } else {
+    printf("Resposta errada\n");
+    printf("Resposta correta: Letra \"C\"\n");
+    errado_1++;
+  }
+
+  getchar();
+  printf("Pressione \"enter\" para continuar...");
+  getchar();
+  system("clear || cls");
+  printf("--------------------------------\n");
+  printf("|  Pergunta 5 - Nivel Facil   |\n");
+  printf("--------------------------------\n\n");
+  printf("O SPAM atualmente em 2012 corresponde a aproximadamente:\n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("A) 50%% de todos os emails \n");
+  printf("B) 40%% de todos os bancos de dados mundiais \n");
+  printf("C) 80%% de todos os provedores de internet \n");
+  printf("D) 32%% da internet \n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("Qual a resposta certa?\n");
+  scanf("%s", &resp);
+  if ((resp == 'a') || (resp == 'A')) {
+    printf("Resposta correta\n");
+    certo_1++;
+  } else {
+    printf("Resposta errada\n");
+    printf("Resposta correta: Letra \"C\"\n");
+    errado_1++;
+  }
+  getchar();
+  printf("Pressione \"enter\" para continuar...");
+  getchar();
+  system("clear || cls");
+  printf("--------------------------------\n");
+  printf("|  Pergunta 6 - Nivel Facil   |\n");
+  printf("--------------------------------\n\n");
+  printf("A velocidade dos processadores atuais eh normalmente medida em:\n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("A) Hertz ou Hz \n");
+  printf("B) Gigahertz ou GHz \n");
+  printf("C) Quilo byte ou Kb \n");
+  printf("D) Megabyte ou MB \n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("Qual a resposta certa?\n");
+  scanf("%s", &resp);
+  if ((resp == 'b') || (resp == 'B')) {
+    printf("Resposta correta\n");
+    certo_1++;
+  } else {
+    printf("Resposta errada\n");
+    printf("Resposta correta: Letra \"C\"\n");
+    errado_1++;
+  }
+  getchar();
+  printf("Pressione \"enter\" para continuar...");
+  getchar();
+  system("clear || cls");
+  printf("--------------------------------\n");
+  printf("|  Pergunta 7 - Nivel Facil   |\n");
+  printf("--------------------------------\n\n");
+  printf("O formato padrao dos arquivos gerados pelo Word eh :\n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("A) txt\n");
+  printf("B) doc \n");
+  printf("C) xls \n");
+  printf("D) html \n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("Qual a resposta certa?\n");
+  scanf("%s", &resp);
+  if ((resp == 'b') || (resp == 'B')) {
+    printf("Resposta correta\n");
+    certo_1++;
+  } else {
+    printf("Resposta errada\n");
+    printf("Resposta correta: Letra \"C\"\n");
+    errado_1++;
+  }
+  getchar();
+  printf("Pressione \"enter\" para continuar...");
+  getchar();
+  system("clear || cls");
+  printf("--------------------------------\n");
+  printf("|  Pergunta 8 - Nivel Facil   |\n");
+  printf("--------------------------------\n\n");
+  printf("A extensao padrao de arquivos de audio no windows eh:\n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("A) WAV \n");
+  printf("B) BIN \n");
+  printf("C) ARC \n");
+  printf("D) WMF \n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("Qual a resposta certa?\n");
+  scanf("%s", &resp);
+  if ((resp == 'a') || (resp == 'A')) {
+    printf("Resposta correta\n");
+    certo_1++;
+  } else {
+    printf("Resposta errada\n");
+    printf("Resposta correta: Letra \"C\"\n");
+    errado_1++;
+  }
+  getchar();
+  printf("Pressione \"enter\" para continuar...");
+  getchar();
+  system("clear || cls");
+  printf("--------------------------------\n");
+  printf("|  Pergunta 9 - Nivel Facil   |\n");
+  printf("--------------------------------\n\n");
+  printf("O numero binario 10101010 equivale a qual numero decimal apresentado "
+         " ?\n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("A) 140\n");
+  printf("B) 150 \n");
+  printf("C) 160\n");
+  printf("D) 170 \n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("Qual a resposta certa?\n");
+  scanf("%s", &resp);
+  if ((resp == 'd') || (resp == 'D')) {
+    printf("Resposta correta\n");
+    certo_1++;
+  } else {
+    printf("Resposta errada\n");
+    printf("Resposta correta: Letra \"C\"\n");
+    errado_1++;
+  }
+  getchar();
+  printf("Pressione \"enter\" para continuar...");
+  getchar();
+  system("clear || cls");
+  printf("--------------------------------\n");
+  printf("|  Pergunta 10 - Nivel Facil   |\n");
+  printf("--------------------------------\n\n");
+  printf(
+      "Eh um tipo de computador que tem como funcao controlar as demais "
+      "estacoes de trabalho quando ligados em uma rede de computadores: ?\n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("A) Estacao de trabalho\n");
+  printf("B) UMPC \n");
+  printf("C) Servidor \n");
+  printf("D) Desktop \n");
+  printf("---------------------------------------------------------------------"
+         "----------\n");
+  printf("Qual a resposta certa?\n");
+  scanf("%s", &resp);
+  if ((resp == 'c') || (resp == 'C')) {
+    printf("Resposta correta\n");
+    certo_1++;
+  } else {
+    printf("Resposta errada\n");
+    printf("Resposta correta: Letra \"C\"\n");
+    errado_1++;
+  }
+  printf("Pressione \"enter\" para continuar...");
   getchar();
   system("clear || cls");
 }
